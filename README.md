0d6e84749918c3a94178fc765191497346383fc5
Vijay Koushik, S <svijaykoushik@hotmail.com>
2017-11-23 23:28:45 +0530

    New featres and documentation added

--- README.md
+++ README.md
@@ -1,2 +1,93 @@
 # device-capabilities.js
-A JavaScript library that detects the client device's support capabilities of several APIs like touch, game pad etc.
+A JavaScript library that detects the client device's capabilities for support of several sensors like touch, motion and orientation sesnors and also provides out of the box support for browser detection and OS detection.
+
+## Description
+Device capabilites.js is a pure javascript library loosely based on the device module in Photonstrom's Phaser framework. The library is quick and simple to use. It can identify if the device is a desktop or mobile, detect the platform the device is running on and find out the name of the browser the library is running on.
+
+## Features
+* Browser detection
+* Platform detection
+* Device detection
+* Feature detection
+
+**Note:** Other than "Feature detection" all other detections are based on the browser's **_User Agent String_**
+
+### Device Detection
+Device capabilities.js libray is able to detect and diffrentiate between *Desktop* and *Mobile* (Tablets are also detected as mobiles)
+
+### Browser Detection
+The library can detect the name of the current browser.
+
+Identifiable browsers are:
+
+**Desktop**
+1. Chrome
+2. Firefox
+3. Internet Explorer
+4. Edge
+5. Opera
+6. Safari
+
+**Mobile Devices**
+1. Chrome for Android and iOS.
+2. Firefox for mobile.
+3. IE Mobile.
+4. Edge Mobile.
+5. Android Native browser.
+6. Silk browser for Kindle.
+
+### Platform Detection
+The library is also able to detect the platform or operating system on which device is running.
+
+Detectable Platforms are:
+
+1. Windows
+2. Linux
+3. Mac OS
+4. Windows Phone
+5. Android
+6. iOS
+7. WebOS
+8. Kindle
+9. Blackberry
+10. Firefox OS
+
+### Feature Detection
+The library has the ability to detect the following features supported by the browser and the device.
+
+1. HTML5 Local storage
+2. HTML5 Canvas
+3. Touch sensor
+4. Geolocation sensor
+5. Motion sensor
+6. Orientation sensor
+7. Ambient light sensor
+
+## Usage
+First, Download the device-capabilites.js library and include it in the html file
+```html
+<script src="path/to/device-capabilities.js"></script>
+```
+Then instantiate the deviceCapabilities class and use its properties for examining the device's capabilities.
+
+Example:
+
+```javascript
+/* create an instace of the deviceCapabilites class*/
+var devAbility = new Device.deviceCapabilities();
+
+// Find out if the device is a desktop
+if(devAbility.desktop){
+    console.log("The device is a desktop");
+}
+
+// Check if the browser is chrome on a mobile device
+if(devAbility.Browser.chromeMobile){
+    console.log("The browser is chrome for mobile");
+}
+
+// Know if device supports touch
+if(devAbility.Sensors.touch){
+    console.log("The device supports touch interface");
+}
+```
--- src/deviceCapabilities.js
+++ src/deviceCapabilities.js
@@ -17,137 +17,137 @@ var Device = {
      */
     deviceCapabilities : function(){
         /**
-         * @property {boolean} is device a desktop?
+         * @property {boolean} desktop - is device a desktop?
+         * @public
          * @default
          */
         this.desktop = false;
         /**
-         * @property {boolean} is touch available?
-         * @default
-         */
-        this.touch = false;
-        /**
-         * @property {boolean} does device support gamepad api?
+         * @property {boolean} gamepad - does device support gamepad api?
+         * @public
          * @default
          */
         this.gamepad = false;
         /**
-         * @property {boolean} is canvas supported?
+         * @property {boolean} canvas - is canvas supported?
+         * @public
          * @default
          */
         this.canvas = false;
         /**
-         * @property {boolean} is device a mobile device?
-         * @default
+         * @property {boolean} mobile - is device a mobile device?
+         * @public
+         * @default [false]
          */
         this.mobile = false;
         /**
-         * @property {boolean} is device running windows?
-         * @default
-         */
-        this.windows = false;
-        /**
-         * @property {boolean} is device running linux?
-         * @default
-         */
-        this.linux = false;
-        /**
-         * @property {boolean} is device running mac?
-         * @default
-         */
-        this.mac = false;
-        /**
-         * @property {boolean} is device running windows phone OS?
-         * @default
-         */
-        this.windowsPhone = false;
-        /**
-         * @property {boolean} is device running android?
-         * @default
-         */
-        this.android = false;
-        /**
-         * @property {boolean} is device running iOS?
-         * @default
-         */
-        this.iOS = false;
-        /**
-         * @property {boolean} is device running WebOS?
-         * @default
-         */
-        this.webOS = false;
-        /**
-         * @property {boolean} is device a Kindle?
-         * @default
-         */
-        this.kindle = false;
-        /**
+         * @property {Object} OS - Object wrapper for device's Operating system
+         * @property {boolean} OS.linux - is device running linux?
+         * @property {boolean} OS.mac - is device running mac?
+         * @property {boolean} OS.windowsPhone - is device running windows phone OS?
+         * @property {boolean} OS.android - is device running android?
+         * @property {boolean} OS.iOS - is device running iOS?
+         * @property {boolean} OS.webOS - is device running WebOS?
+         * @property {boolean} OS.kindle - is device a Kindle?
          * @property {boolean} is device running Blackberry?
+         * @property {boolean} OS.firefoxOS -  is device running Firefox OS?
+         * @public
          * @default
          */
-        this.blackberry = false;
+        this.OS = {
+            windows: false,
+            linux: false,
+            mac: false,
+            windowsPhone: false,
+            android: false,
+            iOS: false,
+            webOS: false,
+            kindle: false,
+            blackberry: false,
+            firefoxOS: false,
+        };
         /**
-         * @property is chrome browser?
+         * @property {Object} Browser - Object wrapper for device's browser.
+         * @property {boolean} Browser.chrome -   is chrome browser?
+         * @property {boolean} Browser.fireFox - is Firefox browser?
+         * @property {boolean} Browser.opera - is Opera browser?
+         * @property {boolean} Browser.opera - is Opera browser?
+         * @property {boolean} Browser.safari - is Safari browser?
+         * @property {boolean} Browser.edge - is Edge browser?
+         * @property {boolean} Browser.ie - is IE browser?
+         * @property {boolean} Browser.chromeMobile - is chrome browser for mobile?
+         * @property {boolean} Browser.fireFoxMobile - is Firefox browser for mobile?
+         * @property {boolean} Browser.ieMobile - is IE browser for mobile?
+         * @property {boolean} Browser.edgeMobile - is edge browser for mobile?
+         * @property {boolean} Browser.androidBrowser - is android default browser?
+         * @property {boolean} Browser.silk - is silk browser for kinlde?
+         * @public
          * @default
          */
-        this.chrome = false;
+        this.Browser = {};
         /**
-         * @property is Firefox browser?
+         * @property {boolean} Browser.chrome -   is chrome browser?
          * @default
          */
-        this.fireFox = false;
+        this.Browser.chrome = false;
         /**
-         * @property is Opera browser?
+         * @property {boolean} Browser.fireFox - is Firefox browser?
          * @default
          */
-        this.opera = false;
+        this.Browser.fireFox = false;
         /**
-         * @property is Safari browser?
+         * @property {boolean} Browser.opera - is Opera browser?
          * @default
          */
-        this.safari = false;
+        this.Browser.opera = false;
         /**
-         * @property is Edge browser?
+         * @property {boolean} Browser.safari - is Safari browser?
          * @default
          */
-        this.edge = false;
+        this.Browser.safari = false;
         /**
-         * @property is IE browser?
+         * @property {boolean} Browser.edge - is Edge browser?
          * @default
          */
-        this.ie = false;
+        this.Browser.edge = false;
         /**
-         * @property is chrome browser for mobile?
+         * @property {boolean} Browser.ie - is IE browser?
          * @default
          */
-        this.chromeMobile = false;
+        this.Browser.ie = false;
         /**
-         * @property is Firefox browser for mobile?
+         * @property {boolean} Browser.chromeMobile - is chrome browser for mobile?
          * @default
          */
-        this.fireFoxMobile = false;
+        this.Browser.chromeMobile = false;
         /**
-         * @property is IE browser for mobile?
+         * @property {boolean} Browser.fireFoxMobile - is Firefox browser for mobile?
          * @default
          */
-        this.ieMobile = false;
+        this.Browser.fireFoxMobile = false;
         /**
-         * @property is edge browser for mobile?
+         * @property {boolean} Browser.ieMobile - is IE browser for mobile?
          * @default
          */
-        this.edgeMobile = false;
+        this.Browser.ieMobile = false;
         /**
-         * @property is android default browser?
+         * @property {boolean} Browser.edgeMobile - is edge browser for mobile?
          * @default
          */
-        this.androidBrowser = false;
+        this.Browser.edgeMobile = false;
         /**
-         * @property is silk browser for kinlde?
+         * @property {boolean} Browser.androidBrowser - is android default browser?
          * @default
          */
-        this.silk = false;
+        this.Browser.androidBrowser = false;
         /**
-         * @property is localstorage supported?
+         * @property {boolean} Browser.silk - is silk browser for kinlde?
+         * @default
+         */
+        this.Browser.silk = false;
+        /**
+         * @property {boolean} localStorage - is localstorage supported?
+         * @public
          * @default
          */
         this.localStorage = false;
@@ -156,44 +156,93 @@ var Device = {
          * @private
          */
         this.ua = navigator.userAgent.toLocaleLowerCase();
+        /**
+         * @property {Object} Sensors - Object wrapper for device's sensors
+         * @property {boolean} Sensors.geoLocation - is geolocation supported?
+         * @property {boolean} Sensors.touch -  is touch available?
+         * @property {boolean} Sensors.orientation -  is orientation sensor available?
+         * @property {boolean} Sensors.motion -  is motion sensor available?
+         * @property {boolean} Sensors.ambientlight -  is ambient light sensor available?
+         * @public
+         * @default
+         */
+        this.Sensors = {};
+
+        /**
+         * @property {boolean} Sensors.geoLocation - is geolocation supported?
+         * @public
+         * @default
+         */
+        this.Sensors.geolocation = false;
+        
+        /**
+         * @property {boolean} Sensors.touch -  is touch available?
+         * @public
+         * @default
+         */
+        this.Sensors.touch = false;
+        
+        /**
+         * @property {boolean} Sensors.orientation -  is orientation sensor available?
+         * @public
+         * @default
+         */
+        this.Sensors.orientation = false;
+        
+        /**
+         * @property {boolean} Sensors.motion -  is motion sensor available?
+         * @public
+         * @default
+         */
+        this.Sensors.motion = false;
+        
+        /**
+         * @property {boolean} Sensors.ambientlight -  is ambient light sensor available?
+         * @public
+         * @default
+         */
+        this.Sensors.ambientlight = false;
 
         // Check for OS of device
-        if(/windows/.test(this.ua)){
-            this.windows = true;
+        if(/firefox/.test(this.ua) && !/windows/.test(this.ua) && !/linux/.test(this.ua) && !/mac os/.test(this.ua) &&!/android/.test(this.ua)){
+            this.OS.firefoxOS = true;
         }
-        else if(/linux/.test(this.ua)){
-            this.linux = true;
-        }
-        else if(/mac os/.test(this.ua)){
-            this.mac = true;
-        }
-        else if(/android/.test(this.ua)){
-            this.android = true;
-        }
-        else if(/ip[oa]d|iphone/.test(this.ua)){
-            this.iOS = true;
+        else if(/windows/.test(this.ua)){
+            this.OS.windows = true;
         }
         else if(/kindle/.test(this.ua) ||/kf[a-z][a-z]+/.test(this.ua) || /silk.*mobile safari/.test(this.ua)){
-            this.kindle = true;
+            this.OS.kindle = true;
+        }
+        else if(/linux/.test(this.ua)){
+            this.OS.linux = true;
+        }
+        else if(/ip[oa]d|iphone/.test(this.ua)){
+            this.OS.iOS = true;
+        }
+        else if(/mac os/.test(this.ua)){
+            this.OS.mac = true;
+        }
+        else if(/android/.test(this.ua)){
+            this.OS.android = true;
         }
         else if(/blackberry|bb/.test(this.ua)){
-            this.blackberry = true;
+            this.OS.blackberry = true;
         }
         else if(/webos/.test(this.ua)){
-            this.webOS = true;
+            this.OS.webOS = true;
         }
 
         //Windows phone gets a seperate check because it conflicts with windows pc check        
         if(/windows phone/.test(this.ua) || /iemobile/.test(this.ua)){
-            this.windowsPhone = true;
-            this.android = false;
-            this.iOS = false;
-            this.windows = false;
-            this.mac = false;
+            this.OS.windowsPhone = true;
+            this.OS.android = false;
+            this.OS.iOS = false;
+            this.OS.windows = false;
+            this.OS.mac = false;
         }
 
         // Check if device is desktop
-        if(this.windows || this.linux || this.mac){
+        if(this.OS.windows || this.OS.linux || this.OS.mac){
             this.desktop = true;
         }
         else{
@@ -201,46 +250,46 @@ var Device = {
         }
 
         //Check for browser
-        if(/edge/.test(this.ua) && !this.windowsPhone){
-            this.edge = true;
+        if(/edge/.test(this.ua) && !this.OS.windowsPhone){
+            this.Browser.edge = true;
         }
         else if(/silk/.test(this.ua)){
-            this.silk = true;
+            this.Browser.silk = true;
         }
         else if(/mozilla\/5\.0/.test(this.ua) && /android/.test(this.ua) && /applewebkit/.test(this.ua) && !/chrome/.test(this.ua)){
-            this.androidBrowser = true;
+            this.Browser.androidBrowser = true;
         }
         else if(/chrome/.test(this.ua) && !/mobile safari|croios/.test(this.ua)){
-            this.chrome = true;
+            this.Browser.chrome = true;
         }
         else if(/firefox/.test(this.ua) && !/mobile|tablet/.test(this.ua)){
-            this.fireFox = true;
+            this.Browser.fireFox = true;
         }
         else if(/msie|trident/.test(this.ua)){
-            this.ie = true;
+            this.Browser.ie = true;
         }
         else if(/opera/.test(this.ua)){
-            this.opera = true;
+            this.Browser.opera = true;
         }
         else if(/safari/.test(this.ua) && !/mobile safari/.test(this.ua)){
-            this.safari = true;
+            this.Browser.safari = true;
         }
-        else if(/edge/.test(this.ua) && this.windowsPhone){
-            this.edgeMobile = true;
+        else if(/edge/.test(this.ua) && this.OS.windowsPhone){
+            this.Browser.edgeMobile = true;
         }
         else if(/chrome.*mobile safari/.test(this.ua) || /chrome.*crios/.test(this.ua)){
-            this.chromeMobile = true;
+            this.Browser.chromeMobile = true;
         }
-        else if(/firefox/.test(this.ua) && /mobile|tablet/.test(this.ua)){
-            this.fireFoxMobile = true;
+        else if(/firefox|fxios/.test(this.ua) && /mobile|tablet/.test(this.ua)){
+            this.Browser.fireFoxMobile = true;
         }
         else if(/iemobile/.test(this.ua) && /mobile|tablet/.test(this.ua)){
-            this.ieMobile = true;
+            this.Browser.ieMobile = true;
         }
 
         //Check touch support
         if('ontouchstart' in window || (window.navigator.maxTouchPoints && window.navigator.maxTouchPoints > 1) || (window.navigator.msMaxTouchPoints && window.navigator.msMaxTouchPoints > 1)){
-            this.touch = true;
+            this.Sensors.touch = true;
         }
 
         //Check gamepad support
@@ -257,5 +306,26 @@ var Device = {
         if(!!localStorage){
             this.localStorage = true;
         }
+
+        //Check geolocation support
+        if(!!navigator.geolocation){
+            this.Sensors.geolocation = true;
+        }
+
+        //Check orientation support
+        if(!!window.ondevicemotion){
+            this.Sensors.orientation = true;
+        }
+        
+        //Check motion support
+        if(!!window.ondevicemotion){
+            this.Sensors.motion = true;
+        }
+        
+        //Check light sensor support
+        if('ondevicelight' in window){
+            this.Sensors.ambientlight = true;
+        }
     }
 };
+//# sourceURL = devicecapabilites.js
